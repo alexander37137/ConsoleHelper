af337ca1e1980e18db5f927950ba3843c3b48d93
Alexandr Andreev <alexandr.andreev@simbirsoft.com>
2015-01-03 16:19:13 +0300

    Fixed bug when multiple commands can't be registered

--- ConsoleHelper/Ioc/OF.cs
+++ ConsoleHelper/Ioc/OF.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,19 +18,7 @@ namespace ConsoleTools.Ioc
         public static void Register<TInterfaceType, TImplementationType>()
            where TImplementationType : TInterfaceType
         {
-            Kernel.Unbind<TInterfaceType>();
             Kernel.Bind<TInterfaceType>().To<TImplementationType>();
-        }
-
-        public static void Register(Type interfaceType, Type classType)
-        {
-            Kernel.Unbind(interfaceType);
-            Kernel.Bind(interfaceType).To(classType);
-        }
-
-        public static T Get<T>()
-        {
-            return Kernel.Get<T>();
         }
 
         public static IEnumerable<TInterfaceType> GetAll<TInterfaceType>()
